a5d105e2b4691b94fdab2ac60fbc91d1639159f9
manul74 <apetrenko74@gmail.com>
2026-04-08 07:11:13 +0300

    fix

--- Models/LItem.cs
+++ Models/LItem.cs
@@ -26,13 +26,11 @@ public partial class LItem : ObservableObject
     public LItem(Guid groupID, Guid itemID, int itemPos, string itemName, string itemCommand1, Boolean anyKeyClose)
     {
         if (itemName == "") itemName = "NewItem";
-        {
-            this.groupID = groupID;
-            this.itemID = itemID;
-            this.itemPos = itemPos;
-            this.itemName = itemName;
-            this.itemCommand1 = itemCommand1;
-            this.anyKeyClose = anyKeyClose;
-        }
+        this.groupID = groupID;
+        this.itemID = itemID;
+        this.itemPos = itemPos;
+        this.itemName = itemName;
+        this.itemCommand1 = itemCommand1;
+        this.anyKeyClose = anyKeyClose;
     }
 }
